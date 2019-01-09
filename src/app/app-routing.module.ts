0b5a38a2856006ffbcf139faa164d6dbf8178491
muthangashem <muthangashem@gmail.com>
2019-01-09 13:59:00 +0300

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BizAddComponent } from './biz-add/biz-add.component'
+import { BizGetComponent } from './biz-get/biz-get.component'
+import { BizEditComponent } from './biz-edit/biz-edit.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+	path: 'business/create',
+	component: BizAddComponent
+},
+{
+	path: 'business/edit/id',
+	component: BizEditComponent
+},
+{
+	path: 'business',
+	component: BizGetComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
