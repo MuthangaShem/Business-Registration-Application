723eef6389b840ce46f295bd39f64f043048bfef
muthangashem <muthangashem@gmail.com>
2019-01-09 15:36:01 +0300

    create service and add as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { BizAddComponent } from './biz-add/biz-add.component';
 import { BizGetComponent } from './biz-get/biz-get.component';
 import { BizEditComponent } from './biz-edit/biz-edit.component';
+import { BusinessService } from './business.service'
 
 @NgModule({
   declarations: [
