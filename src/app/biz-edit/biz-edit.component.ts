72bc861873293f6660f2c00fcb6168d2855b0add
muthangashem <muthangashem@gmail.com>
2019-01-14 12:07:55 +0300

    add edit biz logic to component

--- src/app/biz-edit/biz-edit.component.ts
+++ src/app/biz-edit/biz-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { BusinessService } from '../business.service';
 
 @Component({
   selector: 'app-biz-edit',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BizEditComponent implements OnInit {
 
-  constructor() { }
+	business: any = {};
+	angForm: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+  			  private router: Router,
+  			  private fb: FormBuilder,
+  			  private bs: BusinessService) {
+  			  this.createForm() }
+
+  			  createForm(){
+  			  	this.angForm = this.fb.group({
+  			  		person_name: ['', Validators.required],
+  			  		business_name: ['', Validators.required],
+  			  		business_kra_number: ['', Validators.required]
+  			  	});
+  			  };
+
 
   ngOnInit() {
+  	this.route.params.subscribe(params => {
+  		this.bs.editBusiness(params['id']).subscribe(res => {
+  			this.business = res;
+  		});
+  	});
   }
 
 }
